72f6abdc7fbb13a35583f2e2def3b27cab4fe9c7
jaydeep-kuber <jayofficial085@gmail.coom>
2024-07-19 19:02:09 +0530

    add components & pipeline with py files Viodeo2

---
+++ src/exception.py
@@ -0,0 +1,15 @@
+import sys 
+
+def error_msg_details(error, error_details:sys):
+    _, _, exc_tb = error_details.exc_info()
+    fileName = exc_tb.tb_frame.f_code.co_filename
+    lineNumber = exc_tb.tb_lineno
+    return f"Error occured in script: [{fileName}] at line number: [{lineNumber}] error message: [{error}]" 
+
+class CustomeException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_msg_details(error_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
